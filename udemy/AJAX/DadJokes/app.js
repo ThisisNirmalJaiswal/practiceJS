b1afc6c09e9664a9091b1806797ede3b29072dca
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-03-24 11:48:05 +0530

    dadJokes

--- udemy/AJAX/DadJokes/app.js
+++ udemy/AJAX/DadJokes/app.js
@@ -19,3 +19,30 @@
 // };
 
 // button.addEventListener("click", addNewJoke);
+
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  console.log(jokeText)
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+}
+
+button.addEventListener('click', addNewJoke)
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } }
+    const res = await axios.get("https://icanhazdadjoke.com/", config)
+    return res.data.joke;
+  } catch (err) {
+    return "NO JOKES AVAILABLE! sorry :("
+  }
+}
+
+getDadJoke();
+
