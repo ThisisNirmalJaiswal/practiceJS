43b8ec6de9409dad93f020baaf5cab803dc56b94
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-03-24 13:25:12 +0530

    AJAX

--- udemy/AJAX/TVShowSearch/app.js
+++ udemy/AJAX/TVShowSearch/app.js
@@ -1,19 +1,44 @@
+// const form = document.querySelector('#searchForm');
+// form.addEventListener('submit', async function (e) {
+//     e.preventDefault();
+//     const searchTerm = form.elements.query.value;
+//     const config = { params: { q: searchTerm } }
+//     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+//     makeImages(res.data)
+//     form.elements.query.value = '';
+// })
+
+// const makeImages = (shows) => {
+//     for (let result of shows) {
+//         if (result.show.image) {
+//             const img = document.createElement('IMG');
+//             img.src = result.show.image.medium;
+//             document.body.append(img)
+//         }
+//     }
+// }
+
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
-    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
-    makeImages(res.data)
+    let searchTerm = form.elements.query.value;
+    // console.log(searchTerm)
+    const config = {params: {q: searchTerm}}
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data);
     form.elements.query.value = '';
-})
+});
+
 
 const makeImages = (shows) => {
     for (let result of shows) {
         if (result.show.image) {
             const img = document.createElement('IMG');
-            img.src = result.show.image.medium;
+            // console.log(res.data)
+            img.src = result.show.image.medium
             document.body.append(img)
         }
     }
 }
+
+
