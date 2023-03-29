9b0fa20827aba1e71410f7d34497220b16a967ab
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2023-03-29 22:11:08 +0530

    classes

--- udemy/OOP_Code/05_Classes/app.js
+++ udemy/OOP_Code/05_Classes/app.js
@@ -1,26 +1,63 @@
+// class Color {
+// 	constructor(r, g, b, name) {
+// 		this.r = r;
+// 		this.g = g;
+// 		this.b = b;
+// 		this.name = name;
+// 	}
+// 	innerRGB() {
+// 		const { r, g, b } = this;
+// 		return `${r}, ${g}, ${b}`;
+// 	}
+// 	rgb() {
+// 		return `rgb(${this.innerRGB()})`;
+// 	}
+// 	rgba(a = 1.0) {
+// 		return `rgba(${this.innerRGB()}, ${a})`;
+// 	}
+// 	hex() {
+// 		const { r, g, b } = this;
+// 		return (
+// 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// 		);
+// 	}
+// }
+// const red = new Color(255, 67, 89, 'tomato');
+// const white = new Color(255, 255, 255, 'white');
+
+
+// practice of classes 
 class Color {
-	constructor(r, g, b, name) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.name = name;
-	}
-	innerRGB() {
-		const { r, g, b } = this;
-		return `${r}, ${g}, ${b}`;
-	}
-	rgb() {
-		return `rgb(${this.innerRGB()})`;
-	}
-	rgba(a = 1.0) {
-		return `rgba(${this.innerRGB()}, ${a})`;
-	}
-	hex() {
-		const { r, g, b } = this;
-		return (
-			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-		);
-	}
+    constructor(r, g, b, name) {
+        this.r = r
+        this.g = g
+        this.b = b
+        this.name = name;
+    }
+
+    innerRGB() {
+        const { r, g, b } = this;
+        return `${r}, ${g}, ${b}`;
+    }
+
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB()}, ${a})`
+    }
+
+    hex() {
+        const { r, g, b } = this;
+        return (
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+        )
+    }
 }
-const red = new Color(255, 67, 89, 'tomato');
-const white = new Color(255, 255, 255, 'white');
+
+
+const c1 = new Color(255, 67, 89, 'tomato');
+console.log(c1.rgb())
+
+'#ff4359'
